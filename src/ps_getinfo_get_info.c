f781eca93f2beee1d0ac1fb299c92137e331720c
MoonEgg <kraiwish123@gmail.com>
2023-08-01 16:21:56 +0700

    fix: :sparkles: fix get_line chopchop

--- src/ps_getinfo_get_info.c
+++ src/ps_getinfo_get_info.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ps_getinfo_get_info.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: psaeyang <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: kkaiyawo <kkaiyawo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 15:21:59 by psaeyang          #+#    #+#             */
-/*   Updated: 2023/08/01 16:20:12 by psaeyang         ###   ########.fr       */
+/*   Updated: 2023/08/01 16:21:27 by kkaiyawo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,19 +24,19 @@ void	get_line(char *line, t_vars *parser)
 		erase_split(chop);
 		return ;
 	}
-	if (ft_strncmp(chopchop[0], "A", 2) == 0)
+	if (ft_strncmp(chop[0], "A", 2) == 0)
 		go_get_a(chop, parser);
-	else if (ft_strncmp(chopchop[0], "C", 2) == 0)
+	else if (ft_strncmp(chop[0], "C", 2) == 0)
 		go_get_c(chop, parser);
-	else if (ft_strncmp(chopchop[0], "L", 2) == 0)
+	else if (ft_strncmp(chop[0], "L", 2) == 0)
 		go_get_l(chop, parser);
-	else if (ft_strncmp(chopchop[0], "sp", 3) == 0)
+	else if (ft_strncmp(chop[0], "sp", 3) == 0)
 		go_get_sp(chop, parser);
-	else if (ft_strncmp(chopchop[0], "pl", 3) == 0)
+	else if (ft_strncmp(chop[0], "pl", 3) == 0)
 		go_get_pl(chop, parser);
-	else if (ft_strncmp(chopchop[0], "cy", 3) == 0)
+	else if (ft_strncmp(chop[0], "cy", 3) == 0)
 		go_get_cy(chop, parser);
-	else if (ft_strncmp(chopchop[0], "co", 3) == 0)
+	else if (ft_strncmp(chop[0], "co", 3) == 0)
 		go_get_co(chop, parser);
 	erase_split(chop);
 }
